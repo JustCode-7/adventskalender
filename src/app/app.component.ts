4153beaaa8e4e4e74a0a5f806afd13b70e886593
JustCode-7 <72110343+JustCode-7@users.noreply.github.com>
2022-10-03 21:03:32 +0200

    To do addes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'anvendtskalender-app';
-  backgroundUrl="../assets/pics/baum.jpg"
+  //TODO: change pic per year
+  backgroundUrl = '../assets/pics/baum.jpg';
 }
